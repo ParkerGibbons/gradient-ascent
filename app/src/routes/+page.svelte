b6b7c219d55d3c90b36faf5da10adc26973f41ca
parker gibbons <parkerjgibbons@gmail.com>
2024-06-30 13:09:40 -0700

    add header / github link

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import ControlPanel from '$lib/components/ControlPanel.svelte';
 	import ShaderCode from '$lib/components/ShaderCode.svelte';
 	import { Tabs, TabsContent, TabsList, TabsTrigger } from '$lib/components/ui/tabs';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	const settings = writable({
 		colors: ['#ff0000', '#0000ff'],
@@ -17,12 +18,18 @@
 	let activeTab = 'preview';
 
 	// This function is called whenever `settings` should be updated from `ControlPanel`
-	function updateSettings(newSettings) {
+	function updateSettings(newSettings: typeof $settings) {
 		$settings = { ...newSettings };
 	}
 </script>
 
 <div class="flex h-screen flex-col">
+	<div class="flex justify-between items-center p-4">
+		<h1 class="text-xl font-bold">Gradient Ascent</h1>
+		<Button on:click={() => window.open('https://github.com/ParkerGibbons/gradient-ascent', '_blank')}>
+			View Source on GitHub
+		</Button>
+	</div>
 	<Tabs value={activeTab} onValueChange={(value) => (activeTab = value)} class="w-full pt-3 pl-1">
 		<TabsList>
 			<TabsTrigger value="preview">Preview</TabsTrigger>
